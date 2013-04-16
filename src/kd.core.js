df30e7f105338ee0baf109c060145983412f8c31
Jeremy Kahn <jeremyckahn@gmail.com>
2013-04-16 16:00:17 -0500

    Stubs out the API.

---
+++ src/kd.core.js
@@ -0,0 +1,24 @@
+var kd = {};
+
+
+kd.tick = function () {
+
+};
+
+
+/**
+ * A basic run loop.  `handler` gets called approximately 60 times a second.
+ * @param {function} handler The function to call on every tick.  You almost
+ * certainly want to call `kd.tick` in this function.
+ */
+kd.run = function (handler) {
+
+};
+
+
+/**
+ * Cancels the loop created by `kd.run`.
+ */
+kd.stop = function () {
+
+};
