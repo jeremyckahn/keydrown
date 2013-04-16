ea3af026969110aa0129dd76b8b97df5473a2357
Jeremy Kahn <jeremyckahn@gmail.com>
2013-04-16 16:08:32 -0500

    Adds bootstrap code.

--- src/kd.core.js
+++ src/kd.core.js
@@ -22,3 +22,17 @@ kd.run = function (handler) {
 kd.stop = function () {
 
 };
+
+
+// Bootstrap the library
+if (typeof define === 'function' && define.amd) {
+  // Expose Library as an AMD module if it's loaded with RequireJS or
+  // similar.
+  define(function () {
+    return kd;
+  });
+} else {
+  // Load Library normally (creating a Library global) if not using an AMD
+  // loader.
+  root.kd = kd;
+}
