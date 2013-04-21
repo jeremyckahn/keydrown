6075a9500d19d0a21bffc8bf70e02aec79b01924
Jeremy Kahn <jeremyckahn@gmail.com>
2013-04-20 19:46:22 -0700

    Fixes whitespace from a copy/paste.

--- dist/keydrown.js
+++ dist/keydrown.js
@@ -79,9 +79,9 @@ var util = (function () {
    */
   util.on = function (element, eventName, handler) {
     if (element.addEventListener) {
-      element.addEventListener(eventName, handler, false);
+      element.addEventListener(eventName, handler, false);
     } else if (element.attachEvent) {
-      element.attachEvent('on' + eventName, handler);
+      element.attachEvent('on' + eventName, handler);
     }
   };
 
