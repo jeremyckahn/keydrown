1f157b4275ccdf3818e47cf45b7715ae1944cf17
Andre Z. Sanchez <andrezacsanchez@gmail.com>
2014-02-15 11:52:55 -0500

    Correct case of keycode to keyCode

--- src/kd.key.js
+++ src/kd.key.js
@@ -10,7 +10,7 @@ var Key = (function () {
   /**
    * Represents a key on the keyboard.  You'll never actually call this method directly; Key Objects for every key that Keydrown supports are created for you when the library is initialized (as in, when the file is loaded).  You will, however, use the `prototype` methods below to bind functions to key states.
    *
-   * @param {number} keyCode The keycode of the key.
+   * @param {number} keyCode The keyCode of the key.
    * @constructor
    */
   function Key (keyCode) {
