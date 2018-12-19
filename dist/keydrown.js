2c4ec3940d8ade29ba0c00a31ba3f22a4312660f
Jeremy Kahn <jeremyckahn@gmail.com>
2018-12-19 08:28:41 -0600

    1.2.6

--- dist/keydrown.js
+++ dist/keydrown.js
@@ -1,4 +1,4 @@
-/*! keydrown - v1.2.4 - 2018-12-18 - http://jeremyckahn.github.com/keydrown */
+/*! keydrown - v1.2.5 - 2018-12-19 - http://jeremyckahn.github.com/keydrown */
 ;(function (window) {
 
 var util = (function () {
@@ -141,54 +141,54 @@ var util = (function () {
  * @type {Object.<number>}
  */
 var KEY_MAP = {
-  'ZERO': 48
-  ,'ONE': 49
-  ,'TWO': 50
-  ,'THREE': 51
-  ,'FOUR': 52
-  ,'FIVE': 53
-  ,'SIX': 54
-  ,'SEVEN': 55
-  ,'EIGHT': 56
-  ,'NINE': 57
-  ,'A': 65
-  ,'B': 66
-  ,'C': 67
-  ,'D': 68
-  ,'E': 69
-  ,'F': 70
-  ,'G': 71
-  ,'H': 72
-  ,'I': 73
-  ,'J': 74
-  ,'K': 75
-  ,'L': 76
-  ,'M': 77
-  ,'N': 78
-  ,'O': 79
-  ,'P': 80
-  ,'Q': 81
-  ,'R': 82
-  ,'S': 83
-  ,'T': 84
-  ,'U': 85
-  ,'V': 86
-  ,'W': 87
-  ,'X': 88
-  ,'Y': 89
-  ,'Z': 90
-  ,'ENTER': 13
-  ,'SHIFT': 16
-  ,'ESC': 27
-  ,'SPACE': 32
-  ,'LEFT': 37
-  ,'UP': 38
-  ,'RIGHT': 39
-  ,'DOWN': 40
-  ,'BACKSPACE': 8
-  ,'DELETE': 46
-  ,'TAB': 9
-  ,'TILDE': 192
+  'ZERO': 48,
+  'ONE': 49,
+  'TWO': 50,
+  'THREE': 51,
+  'FOUR': 52,
+  'FIVE': 53,
+  'SIX': 54,
+  'SEVEN': 55,
+  'EIGHT': 56,
+  'NINE': 57,
+  'A': 65,
+  'B': 66,
+  'C': 67,
+  'D': 68,
+  'E': 69,
+  'F': 70,
+  'G': 71,
+  'H': 72,
+  'I': 73,
+  'J': 74,
+  'K': 75,
+  'L': 76,
+  'M': 77,
+  'N': 78,
+  'O': 79,
+  'P': 80,
+  'Q': 81,
+  'R': 82,
+  'S': 83,
+  'T': 84,
+  'U': 85,
+  'V': 86,
+  'W': 87,
+  'X': 88,
+  'Y': 89,
+  'Z': 90,
+  'ENTER': 13,
+  'SHIFT': 16,
+  'ESC': 27,
+  'SPACE': 32,
+  'LEFT': 37,
+  'UP': 38,
+  'RIGHT': 39,
+  'DOWN': 40,
+  'BACKSPACE': 8,
+  'DELETE': 46,
+  'TAB': 9,
+  'TILDE': 192
 };
 
 
